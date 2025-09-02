4a701e0a9a84b008bdc7df043905f0d60117c87d
aklu-10 <24shubhamraj24@gmail.com>
2025-09-02 23:40:15 +0530

    login route

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { lazy } from 'react';
 const Login = lazy(()=>import('./components/auth/Login'))
-const NotFound = lazy(()=>import('./components/auth/NotFound'))
+// const NotFound = lazy(()=>import('./components/auth/NotFound'))
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login/>} />
-        <Route path="*" element={<NotFound/>} />
+        {/* <Route path="*" element={<NotFound/>} /> */}
       </Routes>
     </BrowserRouter>)
 }
