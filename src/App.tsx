c1a6bf8174469ae9a8d19017196e27caae093f0b
aklu-10 <24shubhamraj24@gmail.com>
2025-09-02 23:35:46 +0530

    login route

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login/>} />
+        <Route path="/" element={<Login/>} />
         <Route path="*" element={<NotFound/>} />
       </Routes>
     </BrowserRouter>)
